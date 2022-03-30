6c4863f0c447551c013f5f9ae04bf6a85ac6de62
Benjamin Bergeron <benjamin.bergeron@protonmail.com>
2022-03-30 15:12:47 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'bug_report_ionic_vuejs',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
